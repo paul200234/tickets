582992a440025d2086ac4d2d419e9ad33599a736
paul200234 <paul200234@gmail.com>
2025-11-24 13:38:07 +0100

    js un poco

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function () {
+    $("#ticketSubmit").submit(function (e) {
+        e.preventDefault();
+        let ticketName = $("#ticketName").val();
+        let ticketDescription = $("#ticketDescription").val();
+        let ticketUsername = $("#ticketUsername").val();
+
+
+    })
+})
